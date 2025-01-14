916e178a55781443a194002ceb0094f17733ef88
Martin Andreev <martin_i_a@abv.bg>
2025-01-14 10:39:30 +0200

    Do modifications related with the CORS policy configuration

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 import jsonServer from 'npm:json-server@0.17.4';
 import data from "./data.json" with { type: "json" };
-import cors from "npm:cors";
 
 const server = jsonServer.create()
 
@@ -11,8 +10,13 @@ const server = jsonServer.create()
 // const db = JSON.parse(data);
 const router = jsonServer.router(data)
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 server.use(router)
-server.use(cors())
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
