dcb10a421d79efdb3a54b396e155bface636dc27
Martin Andreev <martin_i_a@abv.bg>
2025-01-14 10:02:34 +0200

    Serve vrscans data

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import jsonServer from 'npm:json-server@0.17.4';
+import data from "./data.json" with { type: "json" };
 
 const server = jsonServer.create()
 
@@ -7,7 +8,7 @@ const server = jsonServer.create()
 // const filePath = path.join('db.json')
 // const data = fs.readFileSync(filePath, "utf-8");
 // const db = JSON.parse(data);
-const router = jsonServer.router({})
+const router = jsonServer.router(data)
 
 server.use(router)
 server.listen(3000, () => {
