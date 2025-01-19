53aa5a59085f47e874f0da66bab4f75374bcfe16
Martin Andreev <martin_i_a@abv.bg>
2025-01-19 14:19:13 +0200

    Do modifications related with the authentication

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
 import jsonServer from 'npm:json-server@0.17.4';
+import auth from 'npm:json-server-auth@2.1.0';
 import data from "./data.json" with { type: "json" };
 
 const server = jsonServer.create()
 
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
 const router = jsonServer.router(data)
+
+const rules = auth.rewriter({
+    vrscans: 644,
+    materials: 644,
+    colors: 644,
+    tags: 644,
+    industries: 644,
+    manufacturers: 644,
+    favorites: 664
+})
+
+server.db = router.db
 
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
@@ -16,7 +24,9 @@ server.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+server.use(rules)
+server.use(auth)
 server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running')
+    console.log('JSON Server is running on port 3000')
 })
