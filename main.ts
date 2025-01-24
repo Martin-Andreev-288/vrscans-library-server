8899b6be9c7ff2e17684b847306429368423f5a2
Martin Andreev <martin_i_a@abv.bg>
2025-01-24 13:32:16 +0200

    Move the filter code from the back end file here
    Now the server returns the filtered results based on criteria, which we'll pass with query parameters.

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
+// @ts-types="npm:@types/json-server@0.14.7"
 import jsonServer from 'npm:json-server@0.17.4';
 import auth from 'npm:json-server-auth@2.1.0';
 import data from "./data.json" with { type: "json" };
+// @ts-types="npm:@types/express@4.17.21"
+import 'npm:express@4.17.1';
+import QueryString from '../../../AppData/Local/deno/npm/registry.npmjs.org/@types/qs/6.9.18/index.d.ts';
 
 const server = jsonServer.create()
 
 const router = jsonServer.router(data)
 
-const rules = auth.rewriter({
+// json-server-auth does not have any types
+const rules = (auth as any).rewriter({
     vrscans: 644,
     materials: 644,
     colors: 644,
@@ -14,9 +19,31 @@ const rules = auth.rewriter({
     industries: 644,
     manufacturers: 644,
     favorites: 664
-})
+});
 
-server.db = router.db
+// required by json-server-auth
+(server as any).db = router.db
+
+
+function readNumberQueryParam(name: string, query: QueryString.ParsedQs): Set<number> {
+    if (!query[name]) return new Set();
+    const elements = Array.isArray(query[name]) ? query[name] : [query[name]];
+    const numberElements = elements.map((color) => parseInt(color as string));
+    return new Set(numberElements);
+}
+
+const filterVrscans = (vrscans: typeof data.vrscans, query: { colors: Set<number>, materials: Set<number>, tags: Set<number> }) =>
+    vrscans.filter((vrscan) => {
+        const acceptsMaterials =
+            !query.materials.size || query.materials.has(vrscan.materialTypeId);
+        const acceptsColors =
+            !query.colors.size ||
+            vrscan.colors.some((color) => query.colors.has(color));
+        const acceptsTags =
+            !query.tags.size || vrscan.tags.some((tag) => query.tags.has(tag));
+
+        return acceptsMaterials && acceptsColors && acceptsTags;
+    });
 
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
@@ -26,7 +53,15 @@ server.use((req, res, next) => {
 });
 server.use(rules)
 server.use(auth)
+server.get('/searchVrscans', (req, res) => {
+    const colors = readNumberQueryParam('colors', req.query);
+    const materials = readNumberQueryParam('materials', req.query);
+    const tags = readNumberQueryParam('tags', req.query);
+    const filteredVrScans = filterVrscans(data.vrscans, { colors, materials, tags });
+    res.send(filteredVrScans);
+})
 server.use(router)
+
 server.listen(3000, () => {
     console.log('JSON Server is running on port 3000')
 })
